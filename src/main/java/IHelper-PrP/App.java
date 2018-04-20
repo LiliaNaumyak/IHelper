f459d0337f9d8e741f80c8704567e31f21fafb04
lnaumiak <lilia.naumyak@gmail.com>
2018-04-20 16:22:35 +0300

    first commit

---
+++ src/main/java/IHelper-PrP/App.java
@@ -0,0 +1,13 @@
+package IHelper-PrP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
